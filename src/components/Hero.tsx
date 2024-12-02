e9ea596bb3948b2ac864536690d00a84b115dfb6
level237 <bramslevel129@gmail>
2024-12-02 16:29:26 +0100

    update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,40 +8,40 @@ export default function Hero() {
     className='w-full z-10  bg-[#02abee]  rounded-md relative h-[85vh] justify-start pt-20 flex flex-col items-center'>
   <div className='absolute bg-[#00723e] h-24 flex gap-5 flex-row justify-center items-center px-12 border border-white border-[0.1rem]  top-[-2rem] z-[9999] '>
           <div className=''>
-            <div className='flex flex-row justify-between items-end gap-5'>
+            <div className='flex flex-row justify-between items-end gap-8'>
                 <h2 className='text-2xl font-semibold text-white'>La FISF</h2>
                 <Separator className='bg-white h-7 w-1' orientation='vertical' />
             </div>
               
           </div>
           <div className=''>
-              <div className='flex flex-row justify-between items-end gap-5'>
-                  <h2 className='text-2xl font-semibold text-white'>Actualités</h2>
+              <div className='flex flex-row justify-between items-end gap-8'>
+                  <h2 className='text-2xl font-semibold ml-3 text-white'>Actualités</h2>
                     <Separator className='bg-white h-7 w-1' orientation='vertical' />
                 </div>
               
           </div>
           <div className=''>
-          <div className='flex flex-row justify-between items-end gap-5'>
-              <h2 className='text-2xl font-semibold text-white'>Calendrier</h2>
+          <div className='flex flex-row justify-between items-end gap-8'>
+              <h2 className='text-2xl font-semibold ml-3 text-white'>Calendrier</h2>
                 <Separator className='bg-white h-7 w-1' orientation='vertical' />
             </div>
           </div>
           <div className=''>
-          <div className='flex flex-row justify-between items-end gap-5'>
-              <h2 className='text-2xl font-semibold text-white'>Classement</h2>
+          <div className='flex flex-row justify-between items-end gap-8'>
+              <h2 className='text-2xl font-semibold ml-3 text-white'>Classement</h2>
                 <Separator className='bg-white h-7 w-1' orientation='vertical' />
             </div>
           </div>
           <div className=''>
-          <div className='flex flex-row justify-between items-end gap-5'>
-              <h2 className='text-2xl font-semibold text-white'>Evènements</h2>
+          <div className='flex flex-row justify-between items-end gap-8'>
+              <h2 className='text-2xl font-semibold ml-3 text-white'>Evènements</h2>
                 <Separator className='bg-white h-7 w-1' orientation='vertical' />
             </div>
           </div>
           <div className=''>
-          <div className='flex flex-row justify-between items-end gap-5'>
-              <h2 className='text-2xl font-semibold text-white'>Forum</h2>
+          <div className='flex flex-row justify-between items-end gap-8'>
+              <h2 className='text-2xl font-semibold ml-3 text-white'>Forum</h2>
                 
             </div>
           </div>
