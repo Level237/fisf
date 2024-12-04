a6d19171299a06c1341ecdb3797a1201cc1ec950
level237 <bramslevel129@gmail.com>
2024-12-04 09:06:18 +0100

    update

--- src/components/MemberForm.tsx
+++ src/components/MemberForm.tsx
@@ -1,4 +1,4 @@
-import tof from "../assets/BG-calendrier.jpg";
+import form from "../assets/form.jpg";
 export const MemberForm=()=>{
 
     return (
@@ -6,15 +6,24 @@ export const MemberForm=()=>{
             <section className="grid grid-cols-2">
                     <div className="relative">
                                 <img
-                            src={tof}
-                            alt="Alexis Allagnat"
+                            src={form}
+                            alt="Contact form"
                             width={200}
                             height={200}
                             className="h-full w-96 object-cover"
                             />
                     </div>
                     <div className="bg-[#00723e] flex flex-col  px-6 py-6">
-                                <h2 className="text-white font-bold text-2xl">Devenez Membre</h2>
+                                <h2 className="text-white font-bold text-3xl">Devenez Membre</h2>
+                                <div className="mt-4 mb-2">
+                                    <input type="text" placeholder="Nom" className="w-full px-6  py-4 placeholder:text-xl rounded-full" />
+                                </div>
+                                <div className="mb-2">
+                                    <input type="text" placeholder="Prénom" className="w-full px-6  py-4 placeholder:text-xl rounded-full" />
+                                </div>
+                                <div className="mb-2">
+                                    <input type="text" placeholder="Prénom" className="w-full px-6  py-4 placeholder:text-xl rounded-full" />
+                                </div>
                     </div>
             </section>
        </section>
--- src/components/figures.tsx
+++ src/components/figures.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import licences from "../assets/licences.png"
+import clubs from "../assets/clubs.png"
+import federation from "../assets/federation.png"
 
 export default function Figures() {
   return (
-    <div className='flex flex-row items-center mt-8 w-full  justify-center'>
-      <div className='flex gap-2 mr-24 flex-col items-center'>
-      <svg width={120} height={120} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#00723e" stroke="#00723e" stroke-width="0.00024000000000000003"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.336"></g><g id="SVGRepo_iconCarrier"><path d="M23 1v18h-3v-1h2V2H2v16h8v1H1V1zm-7 2H8v1h8zm-2 3V5h-4v1zm-7 5H3v1h4zm0 2H3v1h4zm-4 3h2v-1H3zm14-3a2 2 0 1 1-2-2 2.002 2.002 0 0 1 2 2zm-1 0a1 1 0 1 0-1 1 1.001 1.001 0 0 0 1-1zm.002-4.293a.965.965 0 0 0 1.32.55 1.08 1.08 0 0 1 1.213.207 1.066 1.066 0 0 1 .21 1.21.966.966 0 0 0 .548 1.324 1.064 1.064 0 0 1 0 2.004.965.965 0 0 0-.549 1.323A1.05 1.05 0 0 1 18 16.816v7.046l-3-2.538-3 2.538v-7.046a1.05 1.05 0 0 1-.744-1.49.965.965 0 0 0-.549-1.324 1.064 1.064 0 0 1 0-2.004.966.966 0 0 0 .549-1.324 1.066 1.066 0 0 1 .209-1.21 1.08 1.08 0 0 1 1.212-.206.965.965 0 0 0 1.32-.551 1.064 1.064 0 0 1 2.005 0zm.998 13v-5.04a.93.93 0 0 0-.998.625 1.064 1.064 0 0 1-2.004 0 .93.93 0 0 0-.998-.625v5.039l2-1.692zm-1.94-4.749a1.967 1.967 0 0 1 1.853-1.308 2.12 2.12 0 0 1 .87.197l.058-.091a1.964 1.964 0 0 1 1.116-2.695v-.122a1.966 1.966 0 0 1-1.116-2.695l-.087-.084a1.965 1.965 0 0 1-2.694-1.117h-.12a1.965 1.965 0 0 1-2.694 1.117l-.087.084a1.966 1.966 0 0 1-1.116 2.695v.122a1.964 1.964 0 0 1 1.116 2.695l.058.09a2.12 2.12 0 0 1 .87-.196 1.967 1.967 0 0 1 1.853 1.308L15 17z"></path><path fill="none" d="M0 0h24v24H0z"></path></g></svg>
+    <div className='flex flex-row items-center mt-8   justify-center'>
+      <div className='flex gap-2 mr-24 flex-col  items-center'>
+        <img className="w-32 h-32" src={licences} alt="" />
         <div>
             <h2 className='text-5xl font-bold text-red-700'>4 395</h2>
         </div>
@@ -15,7 +17,7 @@ export default function Figures() {
         </div>
       </div>
       <div className='flex gap-2 mr-24 ml-12 flex-col items-center'>
-      <svg  fill="#00723e" width={120} height={120} viewBox="-10.5 -10.5 71.00 71.00" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" stroke="#00723e" stroke-width="0.0005"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round" stroke="#CCCCCC" stroke-width="0.9"></g><g id="SVGRepo_iconCarrier"><path d="M9 0C7.355469 0 6 1.355469 6 3L6 50L44 50L44 3C44 1.355469 42.644531 0 41 0 Z M 9 2L41 2C41.554688 2 42 2.445313 42 3L42 48L38 48L38 36L27 36L27 48L8 48L8 3C8 2.445313 8.445313 2 9 2 Z M 12 6L12 14L23 14L23 6 Z M 27 6L27 14L38 14L38 6 Z M 14 8L21 8L21 12L14 12 Z M 29 8L36 8L36 12L29 12 Z M 12 16L12 24L23 24L23 16 Z M 27 16L27 24L38 24L38 16 Z M 14 18L21 18L21 22L14 22 Z M 29 18L36 18L36 22L29 22 Z M 12 26L12 34L23 34L23 26 Z M 27 26L27 34L38 34L38 26 Z M 14 28L21 28L21 32L14 32 Z M 29 28L36 28L36 32L29 32 Z M 12 36L12 44L23 44L23 36 Z M 14 38L21 38L21 42L14 42 Z M 29 38L36 38L36 48L29 48Z"></path></g></svg>
+          <img className="w-32 h-32" src={federation} alt="" />
       <div>
             <h2 className='text-5xl font-bold text-red-700'>4 395</h2>
         </div>
@@ -26,7 +28,7 @@ export default function Figures() {
         </div>
       </div>
       <div className='flex gap-2 flex-col items-center'>
-      <svg width={120} height={120} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#00723e"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M12 22.943C3.533 18.32 2.498 12.026 3.16 4.723l.08-.869.871-.042c3.31-.166 5.668-.934 7.21-2.35L12 .838l.68.624c1.54 1.416 3.9 2.184 7.21 2.35l.87.042.08.869c.666 7.34-.407 13.61-8.84 18.22zM4.16 4.811c-.543 5.988-.04 12.694 7.838 16.993 7.88-4.3 8.39-11.001 7.845-16.99-3.553-.178-6.12-1.036-7.84-2.616-1.72 1.58-4.29 2.435-7.842 2.613z"></path><path fill="none" d="M0 0h24v24H0z"></path></g></svg>
+          <img className="w-32 h-32" src={clubs} alt="" />
       <div>
             <h2 className='text-5xl font-bold text-red-700'>4 395</h2>
         </div>
